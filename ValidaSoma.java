48d84d862f14fca7816584dd9ccb061538b0c961
jovesposito <jovesposito@outlook.com>
2024-09-04 14:55:47 -0300

    Resolução do exercício de soma

---
+++ ValidaSoma.java
@@ -0,0 +1,17 @@
+
+public class ValidaSoma {
+
+	public static void main(String[] args) {
+		
+		int indice = 12, soma = 0, k = 1;
+	
+		while (k < indice) {
+			k += 1;
+			soma += k;
+		}
+		
+		System.out.println("O valor da variável soma é: " + soma);
+		
+	}
+
+}
